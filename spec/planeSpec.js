69dace849fa07ad1f373d6ac1bcf3fa9eb10edd3
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-02 23:05:40 +0000

    clearForTakeOff method implemented in Airport but currently only emptying the hangar, plane take-off method implemented

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -6,11 +6,17 @@ describe('Plane', function(){
   
   beforeEach(function(){
     plane = new Plane();
-    airport = jasmine.createSpyObj('airport',['clearForLanding']);
+    airport = jasmine.createSpyObj('airport',['clearForLanding', 'clearForTakeOff']);
   });
 
   it('Can land at airport', function(){
-    (plane.land(airport));
+    plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+  });
+
+  it('Can take-off from an airport', function(){
+    plane.land(airport);
+    plane.takeOff();
+    expect(airport.clearForTakeOff).toHaveBeenCalled();
   });
 });
--- src/plane.js
+++ src/plane.js
@@ -1,7 +1,17 @@
 'use strict';
 
 class Plane {
-  land(airport){
-    airport.clearForLanding(this)
+  constructor() {
+    this._location;
   };
+
+  land(airport) {
+    airport.clearForLanding(this);
+    this._location = airport;
+  };
+
+  takeOff() {
+    this._location.clearForTakeOff();
+  };
+
 };
